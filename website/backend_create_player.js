f44a52ffccb242df90e161e7211e202770eb841e
WMowgly <95572447+WMowgly@users.noreply.github.com>
2025-04-17 22:02:22 +0200

    Retour en arriere avant le back cote Mowgly

--- website/backend_create_player.js
+++
@@ -1,9 +0,0 @@
-function getInfoPlayer() {
-    const playerName = document.getElementById('player_name').value;
-    const playerClass = document.getElementById('player_class').value;
-    const playerRace = document.getElementById('player_race').value;
-
-    console.log("Nom :", playerName)
-    console.log("Nom :", playerClass)
-    console.log("Nom :", playerRace)
-  }
